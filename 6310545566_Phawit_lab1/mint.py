f4f01e60aa47ba7515b9aec5b7d11b5c217d1954
helloyeew <gameforphawit@outlook.com>
2021-01-14 15:37:05 +0700

    🎉 init. commit

---
+++ 6310545566_Phawit_lab1/mint.py
@@ -0,0 +1,63 @@
+class Mint:
+    """A mint creates coins by stamping on years.
+
+    The update method sets the mint's stamp to Mint.current_year.
+
+    >>> mint = Mint()
+    >>> mint.year
+    2020
+    >>> dime = mint.create(Dime)
+    >>> dime.year
+    2020
+    >>> Mint.current_year = 2100  # Time passes
+    >>> nickel = mint.create(Nickel)
+    >>> nickel.year     # The mint has not updated its stamp yet
+    2020
+    >>> nickel.worth()  # 5 cents + (80 - 50 years)
+    35
+    >>> mint.update()   # The mint's year is updated to 2100
+    >>> Mint.current_year = 2175     # More time passes
+    >>> mint.create(Dime).worth()    # 10 cents + (75 - 50 years)
+    35
+    >>> Mint().create(Dime).worth()  # A new mint has the current year
+    10
+    >>> dime.worth()     # 10 cents + (155 - 50 years)
+    115
+    >>> Dime.cents = 20  # Upgrade all dimes!
+    >>> dime.worth()     # 20 cents + (155 - 50 years)
+    125
+    """
+    current_year = 2020
+
+    def __init__(self):
+        self.update()
+        self.year = self.current_year
+        self.coin = []
+
+    def create(self, kind):
+        if kind == Nickel:
+            return Nickel(self.year)
+        elif kind == Dime:
+            return Dime(self.year)
+
+    def update(self):
+        self.year = Mint.current_year
+
+
+class Coin:
+    def __init__(self, year):
+        self.year = year
+
+    def worth(self):
+        if Mint.current_year - self.year > 50:
+            return self.cents + ((Mint.current_year - self.year) - 50)
+        else:
+            return self.cents
+
+
+class Nickel(Coin):
+    cents = 5
+
+
+class Dime(Coin):
+    cents = 10
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# helloyeew-lab-computer-programming-ii
-All of my files from Laboratory of Computer Programming II in SKE second term year 1.
+# HelloYeew's Laboratory of Computer Programming II
+
+![GuraLoading](guraloading.gif)
+
+All of my python files from Laboratory of Computer Programming II in SKE second term year 1.
+
+In this repo I put everything in this course into this repo (PDF problem too)
+
+
